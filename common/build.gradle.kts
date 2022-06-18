afe0e86da89152c3d3f915663d662a2ad3aa4d3d
qwq233 <qwq233@qwq2333.top>
2022-06-18 23:31:59 +0800

    update deps
    Signed-off-by: qwq233 <qwq233@qwq2333.top>

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.compose.compose
 
 plugins {
-    id("org.jetbrains.compose")
+    id("org.jetbrains.compose") version "1.1.1"
     kotlin("multiplatform")
     id("com.android.library")
 }
@@ -34,9 +34,9 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 api("androidx.activity:activity-compose:1.4.0")
-                api("androidx.appcompat:appcompat:1.4.0")
-                api("androidx.core:core-ktx:1.7.0")
-                api("com.google.android.material:material:1.6.0-alpha01")
+                api("androidx.appcompat:appcompat:1.4.2")
+                api("androidx.core:core-ktx:1.8.0")
+                api("com.google.android.material:material:1.7.0-alpha02")
 
                 val lifecycleVersion = "2.4.0-alpha03"
                 api("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
@@ -45,11 +45,6 @@ kotlin {
                 val preference_version = "1.1.1"
                 api("androidx.preference:preference:$preference_version")
                 api("androidx.preference:preference-ktx:$preference_version")
-            }
-        }
-        val desktopMain by getting {
-            dependencies {
-                api(compose.desktop.common)
             }
         }
     }
